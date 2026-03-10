9aff65dc17cc41e7b8eef8e5cee2b69bbc02b305
Deyan Terziyski <DETE@milestone.dk>
2026-03-10 15:34:31 +0200

    Add IStorageService to be used for images and audio files

---
+++ src/AudioDelivery.Application/Common/Interfaces/IStorageService.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioDelivery.Application.Common.Interfaces;
+
+/// <summary>
+/// Defines methods for uploading and deleting files in a storage system asynchronously.
+/// </summary>
+/// <remarks>Implementations of this interface provide mechanisms to store and remove files, such as in cloud or
+/// local storage.</remarks>
+public interface IStorageService
+{
+    Task<string> UploadAsync(Stream fileStream, string fileName, string contentType);
+    Task DeleteAsync(string fileUrl);
+}
